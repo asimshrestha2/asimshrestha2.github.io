fdb24b78a0ea7e34a37d6907fd112cacdc91115b
asimshrestha2 <asims178@gmail.com>
2017-09-25 10:22:16 -0400

    Page with hash to open content

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
-  // refresh();
+  if(window.location.hash)
+    openPage(window.location.hash)();
+
+  var pageLinks = document.querySelectorAll("a.page");
+  // console.log(pageLinks);
+
+  function openPage(link: string){
+    return function(){
+      // console.log(link)
+      var current = document.querySelector(".content.current");
+      if(current.getAttribute('id') !== link ){
+        document.querySelector(".content.current").classList.remove('current');
+        document.querySelector(link).classList.add('current');
+      }
+    }
+  }
+  var element;
+  for (var i = 0; i < pageLinks.length; i++) {
+    element = pageLinks[i];
+    element.addEventListener('click', openPage(element.getAttribute('href')), false);
+    // element.onclick = openPage(element.getAttribute('href'));
+  }
 })
