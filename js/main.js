fea14537376469f7313942a8321a18c4fe1459fb
asimshrestha2 <asims178@gmail.com>
2017-09-25 18:05:01 -0400

    Change Style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-window.onload = function(){
-  var titles = document.getElementsByClassName('sec-title');
-  var titlesBottomVal = [0,0,0,0];
-  for(var i = 0; i < titles.length; i++){
-    titlesBottomVal[i] = titles[i].getBoundingClientRect().bottom;
-  }
-
-  document.addEventListener('scroll', function(){
-
-  });
-}
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.hash)
+        openPage(window.location.hash)();
+    var pageLinks = document.querySelectorAll("a.page");
+    function openPage(link) {
+        return function () {
+            var current = document.querySelector(".content.current");
+            if (current.getAttribute('id') !== link) {
+                document.querySelector(".content.current").classList.remove('current');
+                document.querySelector(link).classList.add('current');
+            }
+        };
+    }
+    var element;
+    for (var i = 0; i < pageLinks.length; i++) {
+        element = pageLinks[i];
+        element.addEventListener('click', openPage(element.getAttribute('href')), false);
+    }
+});
